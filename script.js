8d45d7ea10d5d6278228946dc60682fffd5ea07e
José <128156840+josegabrieL17@users.noreply.github.com>
2023-11-19 15:49:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu(contentId) {
+    var content = document.getElementById(contentId);
+    content.style.display = (content.style.display === 'block' || content.style.display === '') ? 'none' : 'block';
+}
+
+
+function handlePedidoClick() {
+    // Substitua esta linha pelo comportamento desejado
+    alert("Seu pedido foi adicionado ao carrinho!");
+}
+
+
+
